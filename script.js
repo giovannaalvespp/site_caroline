6d1b644e3146ce2e4b7f2df3ac6b5c2679f6063b
Giovanna_Rocha <giovannaalvespp@gmail.com>
2020-03-10 13:30:16 -0300

    Criação do website utilizando plataformas de JS, materialize

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ const elemsSidenav = document.querySelectorAll(".sidenav");
 const instancesSidenav = M.Sidenav.init(elemsSidenav, {
     edge: "right"
 });
+var image = document.getElementsByClassName('img1');
+new simpleParallax(image, {
+	orientation: 'right'
+});
+var image = document.getElementsByClassName('img3');
+new simpleParallax(image);
